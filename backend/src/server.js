83e904b9aa4f5e1aa864d81f3099dee8a0480f5a
OLALEKAN SAMSON OGUNDIMU <ogundimuolalekan55@gmail.com>
2026-05-23 23:21:40 +0000

    version 2 of course allocation app

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
 // Health check
